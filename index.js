e726a8a2924567048192ab061f817ea0cf34026f
JorVor <kongphop.kayoonvichien@gmail.com>
2024-08-29 13:49:52 +0700

    3

--- index.js
+++ index.js
@@ -6,26 +6,40 @@ let isRunning = false;
 const countdownDuration = 5 * 60 * 1000;
 
 function start(){
+    /*
+        1.if true will run the block
+        2.set startTime = now-(now-startTime) e.g.13:19-(13:19-13:18) = 00:01
+        3.update timer every 10 milli sec by serInterval function
+    */
     if(!isRunning){
-        startTime = Date.now() - elapsedTime; //now-(now-startTime) like 13:19-(13:19-13:18) = 00:01
+        startTime = Date.now() - elapsedTime;
         remainingTime = countdownDuration;
-        timer = setInterval(update, 10); //update timer every 10 milli sec
+        timer = setInterval(update, 10);
         isRunning = true;
     }
 }
 
 function stop(){
     /*
-        clearInter
+        1.if false will run the block
+        2.stop the repeated execution of the update function by clearInterval function
+        3.update latest elapsedTime
     */
     if(isRunning){
-        clearInterval(timer); //stop the repeated execution of the update function.
+        clearInterval(timer);
         elapsedTime = Date.now() - startTime;
         isRunning = false;
     }
 }
 
-function update(){ //update function get call every 10 milli second by 'setInterval' function
+//function call every 10 milli second by 'setInterval' function on start function
+function update(){
+    /*
+        1.update elapsedTime
+        2.remainingTime is userTimeInput - elapsedTime
+        3.if condition to stop the timer if it's <=0(cannot be negative number)
+        4.convert milliSec to each time then disply
+    */
     const currentTime = Date.now();
     elapsedTime =  currentTime - startTime;
     remainingTime = countdownDuration - elapsedTime;
